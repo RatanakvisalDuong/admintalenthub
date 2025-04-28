ae1ce402a5df906142c22ffb8c747731f2c3578c
RVisalD <126460312+RVisalD@users.noreply.github.com>
2025-04-28 14:41:01 +0700

    changing structure;

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,25 +3,9 @@
 import { useState } from "react";
 import AppBar from "@/component/appbar/appbar";
 import SideBar from "@/component/sidebar/sidebar";
-// import PortfolioManagementPage from "@/component/portfolio-management/page";
-// import EmploymentManagementPage from "@/component/employment-management/page";
-// import UserManagementPage from "@/component/user-management/page";
 
 export default function Layout({children}: {children: React.ReactNode}) {
     const [activeTab, setActiveTab] = useState(0);
-
-    // const renderContent = () => {
-    //     switch (activeTab) {
-    //         case 0:
-    //             return <PortfolioManagementPage />;
-    //         case 1:
-    //             return <EmploymentManagementPage />;
-    //         case 2:
-    //             return <UserManagementPage />;
-    //         default:
-    //             return <PortfolioManagementPage />;
-    //     }
-    // };
 
     return (
         <div className="flex flex-col h-screen text-black">
